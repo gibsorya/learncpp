941d74ee5d1223b4dfd62718204555873bf862fc
Ryan Gibson <ryan@gibs.gg>
2025-07-26 19:55:43 -0500

    lesson 1.11 - first program

---
+++ lessons/multiply-by-two/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int number{ };
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    std::cout << "Double that number is: " << number * 2 << std::endl;
+
+    return 0;
+}
