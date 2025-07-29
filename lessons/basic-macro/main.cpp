2cfa6f4d34acd6226c6569782fb2b44d5661a824
Ryan Gibson <ryan@gibs.gg>
2025-07-29 15:26:26 -0500

    macro with no substitute text

--- lessons/basic-macro/main.cpp
+++ lessons/basic-macro/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
 #define MY_NAME "Ryan"
+#define SAY_MY_NAME
 
 int main() {
+#ifdef SAY_MY_NAME
     std::cout << "My name is " << MY_NAME << std::endl;
+#endif
+
+#ifdef DONT_SAY_MY_NAME
+    std::cout << "My name is not Ryan" << std::endl;
+#endif
 
     return 0;
 }
