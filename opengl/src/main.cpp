2d62a9bfc150ce1682d22b675a2370bf7236c90b
Ryan Gibson <gibsorya@gmail.com>
2025-08-03 22:01:30 -0500

    get glfw window to run

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -3,7 +3,43 @@
 #include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+
+    // Buffers:
+    /*
+    - Front buffer: The buffer that is displayed on the screen.
+    - Back buffer: The buffer that is not displayed on the screen.
+    - Load the back buffer with the new frame.
+    - Swap the front and back buffers to display the frame.
+    */
+
+    glViewport(0, 0, 800, 600);
+
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
-
