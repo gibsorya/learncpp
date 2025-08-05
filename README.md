8a39406295911bc67ee8e69577e544b446317317
Ryan Gibson <ryan@gibs.gg>
2025-08-05 15:39:47 -0500

    basic cmake

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# LEARN CPP LESSONS
+# A general repository of things I've learned w/ C++
+
+I use this repo to store the code I write while learning the C++ language, and for storing the code while exploring topics such as graphics programming (while using C++).
--- lessons/cmake-project/main.cpp
+++ lessons/cmake-project/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "config.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version " << CMakeProject_VERSION_MAJOR << "."
+                  << CMakeProject_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+      }
 
     return 0;
 }
