baa3d6b27a07a2f79ecf7751764dd0c9b66c43cf
Ryan Gibson <ryan@gibs.gg>
2025-08-15 16:03:23 -0500

    Add basic shaders

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -2,116 +2,127 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include"shaderClass.h"
-#include"VAO.h"
-#include"VBO.h"
-#include"EBO.h"
+#include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
 
 // Vertices coordinates
-GLfloat vertices[] =
-{
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+GLfloat vertices[] = {
+    -0.5f,  -0.5f * float(sqrt(3)) / 3,    0.0f, 0.8f, 0.3f,
+    0.02f, // Lower left corner
+    0.5f,   -0.5f * float(sqrt(3)) / 3,    0.0f, 0.8f, 0.3f,
+    0.02f, // Lower right corner
+    0.0f,   0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f,
+    0.32f, // Upper corner
+    -0.25f, 0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f,
+    0.17f, // Inner left
+    0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f,
+    0.17f, // Inner right
+    0.0f,   -0.5f * float(sqrt(3)) / 3,    0.0f, 0.8f, 0.3f,
+    0.02f // Inner down
 };
 
 // Indices for vertices order
-GLuint indices[] =
-{
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+GLuint indices[] = {
+    0, 3, 5, // Lower left triangle
+    3, 2, 4, // Lower right triangle
+    5, 4, 1  // Upper triangle
 };
 
 int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
-    // macOS specific hints
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-        glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
-    #endif
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Window creation
-    GLFWwindow* window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
+// macOS specific hints
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+  glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
+#endif
 
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+  // Window creation
+  GLFWwindow *window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
 
-    glfwMakeContextCurrent(window);
-
-    gladLoadGL();
-
-    // Buffers:
-    /*
-    - Front buffer: The buffer that is displayed on the screen.
-    - Back buffer: The buffer that is not displayed on the screen.
-    - Load the back buffer with the new frame.
-    - Swap the front and back buffers to display the frame.
-    */
-
-    // Set viewport after GLAD is loaded
-    glViewport(0, 0, 800, 800);
-
-    // Generates Shader object using shaders defualt.vert and default.frag
-	Shader shaderProgram("../../../resources/shaders/default.vert", "../../../resources/shaders/default.frag");
-
-    // Create reference containers for the Vartex Array Object and the Vertex Buffer Object
-    // Generates Vertex Array Object and binds it
-	VAO VAO1;
-	VAO1.Bind();
-
-	// Generates Vertex Buffer Object and links it to vertices
-	VBO VBO1(vertices, sizeof(vertices));
-	// Generates Element Buffer Object and links it to indices
-	EBO EBO1(indices, sizeof(indices));
-
-	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
-	// Unbind all to prevent accidentally modifying them
-	VAO1.Unbind();
-	VBO1.Unbind();
-	EBO1.Unbind();
-
-	// Main while loop
-	while (!glfwWindowShouldClose(window))
-	{
-		// Specify the color of the background
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		shaderProgram.Activate();
-		// Bind the VAO so OpenGL knows to use it
-		VAO1.Bind();
-		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
-		// Swap the back buffer with the front buffer
-		glfwSwapBuffers(window);
-		// Take care of all GLFW events
-		glfwPollEvents();
-	}
-
-	// Delete all the objects we've created
-	VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
-	shaderProgram.Delete();
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
-	glfwTerminate();
-
-    glfwDestroyWindow(window);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  gladLoadGL();
+
+  // Buffers:
+  /*
+  - Front buffer: The buffer that is displayed on the screen.
+  - Back buffer: The buffer that is not displayed on the screen.
+  - Load the back buffer with the new frame.
+  - Swap the front and back buffers to display the frame.
+  */
+
+  // Set viewport after GLAD is loaded
+  glViewport(0, 0, 800, 800);
+
+  // Generates Shader object using shaders defualt.vert and default.frag
+  Shader shaderProgram("../../../resources/shaders/default.vert",
+                       "../../../resources/shaders/default.frag");
+
+  // Create reference containers for the Vartex Array Object and the Vertex
+  // Buffer Object Generates Vertex Array Object and binds it
+  VAO VAO1;
+  VAO1.Bind();
+
+  // Generates Vertex Buffer Object and links it to vertices
+  VBO VBO1(vertices, sizeof(vertices));
+  // Generates Element Buffer Object and links it to indices
+  EBO EBO1(indices, sizeof(indices));
+
+  // Links VBO to VAO
+  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void *)0);
+
+  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float),
+                  (void *)(3 * sizeof(float)));
+  // Unbind all to prevent accidentally modifying them
+  VAO1.Unbind();
+  VBO1.Unbind();
+  EBO1.Unbind();
+
+  GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+  // Main while loop
+  while (!glfwWindowShouldClose(window)) {
+    // Specify the color of the background
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    // Clean the back buffer and assign the new color to it
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Tell OpenGL which Shader Program we want to use
+    shaderProgram.Activate();
+
+    glUniform1f(uniID, 0.5f);
+    // Bind the VAO so OpenGL knows to use it
+    VAO1.Bind();
+    // Draw primitives, number of indices, datatype of indices, index of indices
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+    // Swap the back buffer with the front buffer
+    glfwSwapBuffers(window);
+    // Take care of all GLFW events
+    glfwPollEvents();
+  }
+
+  // Delete all the objects we've created
+  VAO1.Delete();
+  VBO1.Delete();
+  EBO1.Delete();
+  shaderProgram.Delete();
+  // Delete window before ending the program
+  glfwDestroyWindow(window);
+  // Terminate GLFW before ending the program
+  glfwTerminate();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
 }
