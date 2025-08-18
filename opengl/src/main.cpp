3dfd6a9596c335f715f15cd529847c98d31b8c3a
Ryan Gibson <ryan@gibs.gg>
2025-08-18 18:44:49 -0400

    added texture to square

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
+
+#define GL_SILENCE_DEPRECATION
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "stb_image.h"
 
-#include "shaderClass.h"
+#include "EBO.h"
 #include "VAO.h"
 #include "VBO.h"
-#include "EBO.h"
+#include "shaderClass.h"
 
 // Vertices coordinates
 GLfloat vertices[] = {
-    -0.5f,  -0.5f * float(sqrt(3)) / 3,    0.0f, 0.8f, 0.3f,
-    0.02f, // Lower left corner
-    0.5f,   -0.5f * float(sqrt(3)) / 3,    0.0f, 0.8f, 0.3f,
-    0.02f, // Lower right corner
-    0.0f,   0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f,
-    0.32f, // Upper corner
-    -0.25f, 0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f,
-    0.17f, // Inner left
-    0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f,
-    0.17f, // Inner right
-    0.0f,   -0.5f * float(sqrt(3)) / 3,    0.0f, 0.8f, 0.3f,
-    0.02f // Inner down
+    //     COORDINATES     /        COLORS      /   TexCoord  //
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Lower left corner
+    -0.5f, 0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // Upper left corner
+    0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Upper right corner
+    0.5f,  -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f  // Lower right corner
 };
 
 // Indices for vertices order
 GLuint indices[] = {
-    0, 3, 5, // Lower left triangle
-    3, 2, 4, // Lower right triangle
-    5, 4, 1  // Upper triangle
+    0, 2, 1, // Upper triangle
+    0, 3, 2  // Lower triangle
 };
 
 int main() {
@@ -81,16 +76,45 @@ int main() {
   EBO EBO1(indices, sizeof(indices));
 
   // Links VBO to VAO
-  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void *)0);
+  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void *)0);
 
-  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float),
+  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float),
                   (void *)(3 * sizeof(float)));
+  VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float),
+                  (void *)(6 * sizeof(float)));
   // Unbind all to prevent accidentally modifying them
   VAO1.Unbind();
   VBO1.Unbind();
   EBO1.Unbind();
 
   GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+  int widthImg, heightImg, numColCh;
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char *bytes = stbi_load("../../../resources/textures/pop_cat.png",
+                                   &widthImg, &heightImg, &numColCh, 0);
+
+  GLuint texture;
+  glGenTextures(1, &texture);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA,
+               GL_UNSIGNED_BYTE, bytes);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  stbi_image_free(bytes);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+  shaderProgram.Activate();
+  glUniform1i(tex0Uni, 0);
 
   // Main while loop
   while (!glfwWindowShouldClose(window)) {
@@ -102,10 +126,11 @@ int main() {
     shaderProgram.Activate();
 
     glUniform1f(uniID, 0.5f);
+    glBindTexture(GL_TEXTURE_2D, texture);
     // Bind the VAO so OpenGL knows to use it
     VAO1.Bind();
     // Draw primitives, number of indices, datatype of indices, index of indices
-    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // Swap the back buffer with the front buffer
     glfwSwapBuffers(window);
     // Take care of all GLFW events
