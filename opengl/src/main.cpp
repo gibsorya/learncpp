0136560e124ebeeda8c4fb7592fe5370c96a2474
Ryan Gibson <ryan@gibs.gg>
2025-08-19 10:24:37 -0400

    going 3Dgit add .

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <pthread.h>
 
 #define GL_SILENCE_DEPRECATION
+#include "stb_image.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "EBO.h"
 #include "VAO.h"
@@ -12,19 +16,29 @@
 #include "textureClass.h"
 
 // Vertices coordinates
-GLfloat vertices[] = {
-    //     COORDINATES     /        COLORS      /   TexCoord  //
-    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Lower left corner
-    -0.5f, 0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // Upper left corner
-    0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Upper right corner
-    0.5f,  -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f  // Lower right corner
+// Vertices coordinates
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
 // Indices for vertices order
-GLuint indices[] = {
-    0, 2, 1, // Upper triangle
-    0, 3, 2  // Lower triangle
+GLuint indices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
+
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 int main() {
   glfwInit();
@@ -39,7 +53,8 @@ int main() {
 #endif
 
   // Window creation
-  GLFWwindow *window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
+  GLFWwindow *window =
+      glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
 
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
@@ -60,7 +75,7 @@ int main() {
   */
 
   // Set viewport after GLAD is loaded
-  glViewport(0, 0, 800, 800);
+  glViewport(0, 0, width, height);
 
   // Generates Shader object using shaders defualt.vert and default.frag
   Shader shaderProgram("../../../resources/shaders/default.vert",
@@ -90,23 +105,51 @@ int main() {
 
   GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
-    Texture popCat("../../../resources/textures/pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-    popCat.texUnit(shaderProgram, "tex0", 0);
+  Texture popCat("../../../resources/textures/pop_cat.png", GL_TEXTURE_2D,
+                 GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+  popCat.texUnit(shaderProgram, "tex0", 0);
+    
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+    
+    // enables depth testing. we need this in order for OpenGL to know to check for depth in a 3D space
+    glEnable(GL_DEPTH_TEST);
   // Main while loop
   while (!glfwWindowShouldClose(window)) {
     // Specify the color of the background
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    // Clean the back buffer and assign the new color to it
-    glClear(GL_COLOR_BUFFER_BIT);
+    // Clean the back buffer and assign the new color to it. also clear the depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Tell OpenGL which Shader Program we want to use
     shaderProgram.Activate();
+
+    double crntTime = glfwGetTime();
+    if(crntTime - prevTime >= 1 / 60) {
+        rotation += 0.5f;
+            prevTime = crntTime;
+    }
+
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 proj = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+    view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+    proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f,
+                            100.0f);
+
+    int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
     glUniform1f(uniID, 0.5f);
     popCat.Bind();
     // Bind the VAO so OpenGL knows to use it
     VAO1.Bind();
     // Draw primitives, number of indices, datatype of indices, index of indices
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
     // Swap the back buffer with the front buffer
     glfwSwapBuffers(window);
     // Take care of all GLFW events
@@ -117,7 +160,7 @@ int main() {
   VAO1.Delete();
   VBO1.Delete();
   EBO1.Delete();
-    popCat.Delete();
+  popCat.Delete();
   shaderProgram.Delete();
   // Delete window before ending the program
   glfwDestroyWindow(window);
